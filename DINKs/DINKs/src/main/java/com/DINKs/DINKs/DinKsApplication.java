c3bbf05076dec48e0c61109e5c35a46ea39631b0
fusdkjfash you <djkolesa@gmail.com>
2023-03-18 13:22:56 -0700

    Spring initializr creating Spring boot application

---
+++ DINKs/DINKs/src/main/java/com/DINKs/DINKs/DinKsApplication.java
@@ -0,0 +1,13 @@
+package com.DINKs.DINKs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinKsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinKsApplication.class, args);
+	}
+
+}
---
+++ DINKs/DINKs/src/test/java/com/DINKs/DINKs/DinKsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DINKs.DINKs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinKsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
