7f5f8f191c80efeb4c3bba7e60bf2b33b70d70b7
fusdkjfash you <djkolesa@gmail.com>
2023-03-18 22:09:22 -0700

    Added Servlet initializer and application / bean creation

--- DINKs/DINKs/src/main/java/com/DINKs/DinksApplication.java
+++ DINKs/DINKs/src/main/java/com/DINKs/DinksApplication.java
@@ -1,14 +1,14 @@
-package com.dinks.dinks;
+package com.dinks;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-
+	
 @SpringBootApplication
 public class DinksApplication {
 
--- DINKs/DINKs/src/main/java/com/DINKs/ServletInitializer.java
+++ DINKs/DINKs/src/main/java/com/DINKs/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.dinks.dinks;
+package com.dinks;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
